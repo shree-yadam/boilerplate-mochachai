ead678a1d194d73605c4c166f43494531aff60fd
Shree Devi Yadam <shree.yadam@gmail.com>
2021-08-24 00:53:11 -0400

    Test case 25

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -65,10 +65,9 @@ suite('Functional Tests', function () {
 });
 
 const Browser = require('zombie');
-Browser.localhost('https://serene-forest-22269.herokuapp.com', process.env.PORT || 3000);
+Browser.site = 'hhttps://serene-forest-22269.herokuapp.com';
 
 suite('Functional Tests with Zombie.js', function () {
-  this.timeout(5000);
   const browser = new Browser();
 
   suiteSetup(function(done) {
@@ -83,14 +82,30 @@ suite('Functional Tests with Zombie.js', function () {
 
   suite('"Famous Italian Explorers" form', function () {
     // #5
+
     test('Submit the surname "Colombo" in the HTML form', function (done) {
-      assert.fail();
+      browser.fill('surname',    'Colombo')
+        .then(() => {
+          browser.pressButton('submit', function(){
+            browser.assert.success();
+            browser.assert.text('#name', 'Cristoforo');
+            browser.assert.text('#surname', 'Colombo');
+          });
+        });
 
       done();
     });
     // #6
     test('Submit the surname "Vespucci" in the HTML form', function (done) {
-      assert.fail();
+      browser.fill('surname',    'Vespucci')
+        .then(() => {
+          browser.pressButton('submit', function(){
+            browser.assert.success();
+            browser.assert.text('#name', 'Amerigo');
+            browser.assert.text('#surname', 'Vespucci');
+          });
+        });
+
 
       done();
     });
